8432baf626bc41d53aa21935a62ad71af4622b29
Lucas Matheus <lucasmatheus2021@gmail.com>
2022-08-24 01:37:43 -0300

    fix: tls redis prod ]error
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/providers/redis/redis.module.ts
+++ src/providers/redis/redis.module.ts
@@ -13,9 +13,7 @@ import * as redisStore from 'cache-manager-ioredis';
         store: redisStore,
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
-        tls: {
-          rejectUnauthorized: false
-        },
+        tls: false,
         ttl: 15
       })
     })
